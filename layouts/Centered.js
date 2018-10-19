6c4a53f37a820d0b4d42a8a1dc9813742b27c07b
Johan Leino <johan.leino@aptitud.se>
2018-10-19 16:44:36 +0200

    version 1.2

---
+++ layouts/Centered.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+    width: 100vw
+    height: 100vh
+`;
+
+const Center = styled.div`
+    > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+const Centered = ({ children }) => {
+    return (
+        <Root>
+            <Center>{children}</Center>
+        </Root>
+    );
+};
+
+export default Centered;
